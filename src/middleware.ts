8ede24aba7eb7d90762068e212d3a544aceae8f5
PRITAM RAHA <rahapritam21@yahoo.com>
2025-07-31 16:18:17 +0530

    seventh

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,18 @@
 // src/middleware.ts
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // Define the routes that should be accessible to everyone, even signed-out users.
 const isPublicRoute = createRouteMatcher([
-  '/', // The homepage is public
-  '/sign-in(.*)', // The sign-in page and all of its sub-pages
-  '/sign-up(.*)', // The sign-up page and all of its sub-pages
-  '/api/webhooks/clerk', // The webhook receiver is public
+  "/", // The homepage is public
+  "/sign-in(.*)", // The sign-in page and all of its sub-pages
+  "/sign-up(.*)", // The sign-up page and all of its sub-pages
+  "/api/webhooks/clerk", // The webhook receiver is public
 ]);
 
 export default clerkMiddleware((auth, request) => {
   // If the request is not for a public route, protect it.
   // This will require the user to be signed in.
+  console.log("⚡ Middleware executing on route:", request.nextUrl.pathname);
   if (!isPublicRoute(request)) {
     auth();
   }
